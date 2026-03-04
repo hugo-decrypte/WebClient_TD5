e618f1ffd8c9b61292e8bbc2b350cf5f2546cce1
Hugo Bena <heno.drawing@gmail.com>
2026-03-04 11:21:16 +0100

    Exercice 5: création des routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/Cagnottes.vue'
+import NewCagnotteView from '../views/NewCagnotte.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/cagnottes/new',
+      name: 'new-cagnotte',
+      component: NewCagnotteView,
+    }
+  ],
 })
 
 export default router
